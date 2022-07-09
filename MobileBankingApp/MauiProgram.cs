d3ddff4de741479f65c673e23e498120e5c46874
tunji hammed <nahtlosng@gmail.com>
2022-07-09 13:13:09 -0700

    Add project files.

---
+++ MobileBankingApp/MauiProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace MobileBankingApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("fa-regular-400.ttf", "IconSet");
+				fonts.AddFont("Rubik-Bold.ttf", "BoldFont");
+				fonts.AddFont("Rubik-Medium.ttf", "MediumFont");
+				fonts.AddFont("Rubik-Regular.ttf", "RegularFont");
+				fonts.AddFont("Rubik-Semibold.ttf", "SemiboldFont");
+            });
+
+		return builder.Build();
+	}
+}
